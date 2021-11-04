db315f90822390199f502cedd09a4394a7a10f78
gautamomento <gautam@momentohq.com>
2021-11-04 10:36:10 -0700

    fix: Push always uses default version

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 import setuptools
 import os
-import time
 
 version = os.getenv("MOMENTO_SDK_VERSION")
 
 if [version == None]:
     version = '0.0.dev'
+    print('version:' + version)
 
 # version is the only dynamic configuration
 setuptools.setup(
