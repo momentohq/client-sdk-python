04c0faec725f0e88dd358982760e1ee29350cbf0
erikatharp <eem@momentohq.com>
2022-01-11 12:46:22 -0800

    add docstrings to Momento class methods

--- src/momento/momento.py
+++ src/momento/momento.py
@@ -16,6 +16,12 @@ from . import _momento_logger
 
 class Momento:
     def __init__(self, auth_token, endpoint_override=None):
+        """Inits Momento to setup SCS client.
+
+        Args: 
+            auth_token: Momento JWT
+            endpoint_override: String of optional endpoint ovveride to be used when given an explicit endpoint by the Momneto team
+        """
         endpoints = _momento_endpoint_resolver.resolve(auth_token,
                                                        endpoint_override)
         self._auth_token = auth_token
@@ -36,6 +42,17 @@ class Momento:
         self._secure_channel.close()
 
     def create_cache(self, cache_name):
+        """Creates a new cache in your Momento account.
+
+        Args: 
+            cache_name: String used to create cache.
+
+        Returns:
+            CreateCacheResponse
+
+        Raises:
+            Exception to notify cache creation failure.
+        """
         try:
             _momento_logger.debug(f'Creating cache with name: {cache_name}')
             request = CreateCacheRequest()
@@ -46,6 +63,17 @@ class Momento:
             raise _cache_service_errors_converter.convert(e) from None
 
     def delete_cache(self, cache_name):
+        """Deletes a cache and all of the items within it.
+
+        Args: 
+            cache_name: String cache name to delete.
+
+        Returns:
+            DeleteCacheResponse
+
+        Raises:
+            Exception to notify cache deletion failure.
+        """
         try:
             _momento_logger.debug(f'Deleting cache with name: {cache_name}')
             request = DeleteCacheRequest()
@@ -56,6 +84,19 @@ class Momento:
             raise _cache_service_errors_converter.convert(e) from None
 
     def get_cache(self, cache_name, ttl_seconds, create_if_absent=False):
+        """Gets a MomentoCache to perform gets and sets on.
+
+        Args: 
+            cache_name: String cache name
+            ttl_seconds: Time to live if object insdie of cache in seconds.
+            create_if_absent: Boolean value to decide if cahce should be created if it does not exist.
+
+        Returns:
+            Cache
+
+        Raises:
+            CacheNotFoundError
+        """
         cache = Cache(self._auth_token, cache_name, self._cache_endpoint,
                      ttl_seconds)
         try:
@@ -69,6 +110,17 @@ class Momento:
         return cache._connect()
 
     def list_caches(self, next_token=None):
+        """Lists ll caches.
+
+        Args: 
+            next_token: Token to continue paginating through the list. It's ised to hnadle large paginated lists.
+
+        Returns:
+            ListCachesResponse
+
+        Raises:
+            Exception to notify either sdk, grpc, or operation error.
+        """
         try:
             list_caches_request = ListCachesRequest()
             list_caches_request.next_token = next_token if next_token is not None else ''
