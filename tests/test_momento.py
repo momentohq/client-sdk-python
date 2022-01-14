a976c2ee6a96f9d3a41562956b6236b3a773724b
gautamomento <gautam@momentohq.com>
2022-01-14 13:09:46 -0800

    format

--- tests/test_momento.py
+++ tests/test_momento.py
@@ -5,25 +5,29 @@ import uuid
 import momento.simple_cache_client as simple_cache_client
 from momento.cache_operation_responses import CacheResult
 
-
 _AUTH_TOKEN = os.getenv('TEST_AUTH_TOKEN')
 _TEST_CACHE_NAME = os.getenv('TEST_CACHE_NAME')
 _DEFAULT_TTL_SECONDS = 60
 
-class TestMomento(unittest.TestCase):
 
+class TestMomento(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         if not _AUTH_TOKEN:
-            raise RuntimeError("Integration tests require TEST_AUTH_TOKEN env var; see README for more details.")
+            raise RuntimeError(
+                "Integration tests require TEST_AUTH_TOKEN env var; see README for more details."
+            )
         if not _TEST_CACHE_NAME:
-            raise RuntimeError("Integration tests require TEST_CACHE_NAME env var; see README for more details.")
+            raise RuntimeError(
+                "Integration tests require TEST_CACHE_NAME env var; see README for more details."
+            )
 
     def test_happy_path(self):
         key = str(uuid.uuid4())
         value = str(uuid.uuid4())
 
-        with simple_cache_client.get(_AUTH_TOKEN, _DEFAULT_TTL_SECONDS) as simple_cache:
+        with simple_cache_client.get(_AUTH_TOKEN,
+                                     _DEFAULT_TTL_SECONDS) as simple_cache:
             simple_cache.set(_TEST_CACHE_NAME, key, value)
             get_resp = simple_cache.get(_TEST_CACHE_NAME, key)
 
