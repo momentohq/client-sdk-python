f4bf1b37f1100cd88629a2ac7293770f15892473
erikatharp <eem@momentohq.com>
2022-02-09 15:19:55 -0800

    chore: delete PYPIREADME and use single line long description; update tox.ini and folder name

--- setup.py
+++ setup.py
@@ -6,13 +6,10 @@ version = os.getenv("MOMENTO_SDK_VERSION")
 if(version is None):
     version = '0.0.0+localdev0'
 
-with open("PYPIREADME.md", "r") as fh:
-    long_description = fh.read()
-
 # version is the only dynamic configuration
 setuptools.setup(
     version=version,
-    long_description=long_description,
+    long_description="# Momento SDK\n Check out our SDK example [here](https://github.com/momentohq/client-sdk-examples/tree/main/python)!",
     long_description_content_type="text/markdown",
     project_urls={
         'Source': 'https://github.com/momentohq/client-sdk-python',
