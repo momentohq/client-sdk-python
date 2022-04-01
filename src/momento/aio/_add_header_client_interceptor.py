d2ebf0b4fb2387aefc57bc3c21eb2f39141dc15b
Erika Tharp <eem@momentohq.com>
2022-04-01 12:47:12 -0700

    chore: Add `agent` metadata (#75)

--- src/momento/aio/_add_header_client_interceptor.py
+++ src/momento/aio/_add_header_client_interceptor.py
@@ -1,13 +1,27 @@
-from typing import Callable, Union
+from typing import Callable, Union, List
 
 import grpc
 from grpc.aio import Metadata
 
 
+class Header:
+    once_only_headers = ["agent"]
+
+    def __init__(self, name: str, value: str):
+        self.name = name
+        self.value = value
+
+
 class AddHeaderClientInterceptor(grpc.aio.UnaryUnaryClientInterceptor):
-    def __init__(self, header_name: str, header_value: str):
-        self._header_name = header_name
-        self._header_value = header_value
+    are_only_once_headers_sent = False
+
+    def __init__(self, headers: List[Header]):
+        self._headers_to_add_once = list(
+            filter(lambda header: header.name in header.once_only_headers, headers)
+        )
+        self.headers_to_add_every_time = list(
+            filter(lambda header: header.name not in header.once_only_headers, headers)
+        )
 
     async def intercept_unary_unary(
         self,
@@ -20,6 +34,11 @@ class AddHeaderClientInterceptor(grpc.aio.UnaryUnaryClientInterceptor):
     ) -> Union[grpc.aio._call.UnaryUnaryCall, grpc.aio._typing.ResponseType]:
         if client_call_details.metadata is None:
             client_call_details.metadata = Metadata()
-        client_call_details.metadata.add(self._header_name, self._header_value)
+        for header in self.headers_to_add_every_time:
+            client_call_details.metadata.add(header.name, header.value)
+        if AddHeaderClientInterceptor.are_only_once_headers_sent == False:
+            for header in self._headers_to_add_once:
+                client_call_details.metadata.add(header.name, header.value)
+            AddHeaderClientInterceptor.are_only_once_headers_sent = True
 
         return await continuation(client_call_details, request)
--- src/momento/aio/_scs_grpc_manager.py
+++ src/momento/aio/_scs_grpc_manager.py
@@ -1,19 +1,27 @@
 import grpc
+import pkg_resources
 
 import momento_wire_types.cacheclient_pb2_grpc as cache_client
 import momento_wire_types.controlclient_pb2_grpc as control_client
 
 from ._add_header_client_interceptor import AddHeaderClientInterceptor
+from ._add_header_client_interceptor import Header
 
 
 class _ControlGrpcManager:
     """Momento Internal."""
 
+    version = pkg_resources.get_distribution("momento").version
+
     def __init__(self, auth_token: str, endpoint: str):
+        headers = [
+            Header("authorization", auth_token),
+            Header("agent", f"python:{_ControlGrpcManager.version}"),
+        ]
         self._secure_channel = grpc.aio.secure_channel(
             target=endpoint,
             credentials=grpc.ssl_channel_credentials(),
-            interceptors=[AddHeaderClientInterceptor("authorization", auth_token)],
+            interceptors=[AddHeaderClientInterceptor(headers)],
         )
 
     async def close(self) -> None:
@@ -26,11 +34,17 @@ class _ControlGrpcManager:
 class _DataGrpcManager:
     """Momento Internal."""
 
+    version = pkg_resources.get_distribution("momento").version
+
     def __init__(self, auth_token: str, endpoint: str):
+        headers = [
+            Header("authorization", auth_token),
+            Header("agent", f"python:{_ControlGrpcManager.version}"),
+        ]
         self._secure_channel = grpc.aio.secure_channel(
             target=endpoint,
             credentials=grpc.ssl_channel_credentials(),
-            interceptors=[AddHeaderClientInterceptor("authorization", auth_token)],
+            interceptors=[AddHeaderClientInterceptor(headers)],
         )
 
     async def close(self) -> None:
