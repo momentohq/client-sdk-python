d0e5b58290acb06b6177592cecb93e6822a5e1ef
Michael Landis <michael@momentohq.com>
2022-09-09 18:53:36 -0700

    Port exists tests to pytest

--- tests/incubating/test_exists.py
+++ tests/incubating/test_exists.py
@@ -1,7 +1,11 @@
 import os
 import unittest
 
+import pytest
+
+import momento.errors as errors
 import momento.incubating.simple_cache_client as simple_cache_client
+from momento.incubating.simple_cache_client import SimpleCacheClient
 from tests.utils import uuid_str
 
 _AUTH_TOKEN = os.getenv("TEST_AUTH_TOKEN")
@@ -9,77 +13,78 @@ _TEST_CACHE_NAME = os.getenv("TEST_CACHE_NAME")
 _DEFAULT_TTL_SECONDS = 60
 
 
-class TestMomento(unittest.TestCase):
-    def test_exists_unary_missing(self):
-        with simple_cache_client.init(
-            _AUTH_TOKEN, _DEFAULT_TTL_SECONDS
-        ) as simple_cache:
-            key = uuid_str()
-            response = simple_cache.exists(_TEST_CACHE_NAME, key)
+@pytest.fixture
+def client() -> SimpleCacheClient:
+    if not _AUTH_TOKEN:
+        raise RuntimeError(
+            "Integration tests require TEST_AUTH_TOKEN env var; see README for more details."
+        )
+    if not _TEST_CACHE_NAME:
+        raise RuntimeError(
+            "Integration tests require TEST_CACHE_NAME env var; see README for more details."
+        )
 
-            self.assertFalse(response)
-            self.assertEqual(0, response.num_exists())
-            self.assertEqual([False], response.results())
-            self.assertEqual([key], response.missing_keys())
-            self.assertEqual([], response.present_keys())
-            self.assertEqual(
-                [(key, False)], list(response.zip_keys_and_results())
-            )
+    with simple_cache_client.init(_AUTH_TOKEN, _DEFAULT_TTL_SECONDS) as _client:
+        # Ensure test cache exists
+        try:
+            _client.create_cache(_TEST_CACHE_NAME)
+        except errors.AlreadyExistsError:
+            pass
 
-    def test_exists_unary_exists(self):
-        with simple_cache_client.init(
-            _AUTH_TOKEN, _DEFAULT_TTL_SECONDS
-        ) as simple_cache:
-            key, value = uuid_str(), uuid_str()
-            simple_cache.set(_TEST_CACHE_NAME, key, value)
-
-            response = simple_cache.exists(_TEST_CACHE_NAME, key)
-
-            self.assertTrue(response)
-            self.assertEqual(1, response.num_exists())
-            self.assertEqual([True], response.results())
-            self.assertEqual([], response.missing_keys())
-            self.assertEqual([key], response.present_keys())
-            self.assertEqual(
-                [(key, True)], list(response.zip_keys_and_results())
-            )
-
-    def test_exists_multi(self):
-        with simple_cache_client.init(
-            _AUTH_TOKEN, _DEFAULT_TTL_SECONDS
-        ) as simple_cache:
-            keys = []
-            for i in range(3):
-                key = uuid_str()
-                simple_cache.set(_TEST_CACHE_NAME, key, uuid_str())
-                keys.append(key)
-
-            response = simple_cache.exists(_TEST_CACHE_NAME, *keys)
-            self.assertTrue(response)
-            self.assertTrue(response.all())
-            self.assertEqual(3, response.num_exists())
-            self.assertEqual([True] * 3, response.results())
-            self.assertEqual([], response.missing_keys())
-            self.assertEqual(keys, response.present_keys())
-            self.assertEqual(
-                list(zip(keys, [True] * 3)), list(response.zip_keys_and_results())
-            )
-
-            missing1, missing2 = uuid_str(), uuid_str()
-            more_keys = [missing1] + keys + [missing2]
-            response = simple_cache.exists(_TEST_CACHE_NAME, *more_keys)
-            self.assertFalse(response)
-            self.assertFalse(response.all())
-            self.assertEqual(3, response.num_exists())
-            mask = [False] + [True] * 3 + [False]
-            self.assertEqual(mask, response.results())
-
-            self.assertEqual([missing1, missing2], response.missing_keys())
-            self.assertEqual(keys, response.present_keys())
-            self.assertEqual(
-                list(zip(more_keys, mask)), list(response.zip_keys_and_results())
-            )
+        yield _client
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_exists_unary_missing(client: SimpleCacheClient):
+    key = uuid_str()
+    response = client.exists(_TEST_CACHE_NAME, key)
+    assert not response
+    assert response.num_exists() == 0
+    assert response.results() == [False]
+    assert response.missing_keys() == [key]
+    assert response.present_keys() == []
+    assert list(response.zip_keys_and_results()) == [(key, False)]
+
+
+def test_exists_unary_exists(client: SimpleCacheClient):
+    key, value = uuid_str(), uuid_str()
+    client.set(_TEST_CACHE_NAME, key, value)
+
+    response = client.exists(_TEST_CACHE_NAME, key)
+
+    assert response
+    assert response.num_exists() == 1
+    assert response.results() == [True]
+    assert response.missing_keys() == []
+    assert response.present_keys() == [key]
+    assert list(response.zip_keys_and_results()) == [(key, True)]
+
+
+def test_exists_multi(client: SimpleCacheClient):
+    keys = []
+    for i in range(3):
+        key = uuid_str()
+        client.set(_TEST_CACHE_NAME, key, uuid_str())
+        keys.append(key)
+
+    response = client.exists(_TEST_CACHE_NAME, *keys)
+    assert response
+    assert response.all()
+    assert response.num_exists() == 3
+    assert response.results() == [True] * 3
+    assert response.missing_keys() == []
+    assert response.present_keys() == keys
+    assert list(response.zip_keys_and_results()) == list(zip(keys, [True] * 3))
+
+    missing1, missing2 = uuid_str(), uuid_str()
+    more_keys = [missing1] + keys + [missing2]
+    response = client.exists(_TEST_CACHE_NAME, *more_keys)
+    assert not response
+    assert not response.all()
+    assert response.num_exists() == 3
+
+    mask = [False] + [True] * 3 + [False]
+    assert response.results() == mask
+
+    assert response.missing_keys() == [missing1, missing2]
+    assert response.present_keys() == keys
+    assert list(response.zip_keys_and_results()) == list(zip(more_keys, mask))
