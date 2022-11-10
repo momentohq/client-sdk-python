9ab32af4e771c26a34c334f03a964add11c09475
eaddingtonwhite <5491827+ellery44@users.noreply.github.com>
2022-11-10 11:29:54 -0800

    fix: workaround to make our grpc interceptor more flexible

--- src/momento/aio/_add_header_client_interceptor.py
+++ src/momento/aio/_add_header_client_interceptor.py
@@ -1,7 +1,7 @@
 from typing import Callable, List, Union
 
 import grpc
-from grpc.aio import Metadata
+from grpc.aio import Metadata, ClientCallDetails
 
 
 class Header:
@@ -24,21 +24,59 @@ class AddHeaderClientInterceptor(grpc.aio.UnaryUnaryClientInterceptor):
         )
 
     async def intercept_unary_unary(
-        self,
-        continuation: Callable[
-            [grpc.aio._interceptor.ClientCallDetails, grpc.aio._typing.RequestType],
-            grpc.aio._call.UnaryUnaryCall,
-        ],
-        client_call_details: grpc.aio._interceptor.ClientCallDetails,
-        request: grpc.aio._typing.RequestType,
+            self,
+            continuation: Callable[
+                [grpc.aio._interceptor.ClientCallDetails, grpc.aio._typing.RequestType],
+                grpc.aio._call.UnaryUnaryCall,
+            ],
+            client_call_details: grpc.aio._interceptor.ClientCallDetails,
+            request: grpc.aio._typing.RequestType,
     ) -> Union[grpc.aio._call.UnaryUnaryCall, grpc.aio._typing.ResponseType]:
+
+        # FIXME Hack to account for ddtrace lib injecting grpc metadata as list instead of proper grpc.aio.Metadata()
+        # Will open issue on data dog repo to discuss this more.
+        # Since once client_call_details.metadata is set as list we cant overwrite with `Metadata()` class we build
+        # up a new ClientCallDetails() in each request now in this interceptor and make sure Metadata() is set properly
+        new_client_call_details = None
+
+        # If no metadata set on passed in client call details then we are first to set, so we should just initialize
         if client_call_details.metadata is None:
-            client_call_details.metadata = Metadata()
+            new_client_call_details = ClientCallDetails(
+                method=client_call_details.method,
+                timeout=client_call_details.timeout,
+                metadata=Metadata(),
+                credentials=client_call_details.credentials,
+                wait_for_ready=client_call_details.wait_for_ready,
+            )
+
+        # This is block hit when ddtrace interceptor runs first and sets metadata as a list
+        elif isinstance(client_call_details.metadata, list):
+            existing_headers = client_call_details.metadata
+            metadata = Metadata()
+            # re-add all existing values to new metadata
+            for md_key, md_value in existing_headers:
+                metadata.add(md_key, md_value)
+            new_client_call_details = ClientCallDetails(
+                method=client_call_details.method,
+                timeout=client_call_details.timeout,
+                metadata=metadata,
+                credentials=client_call_details.credentials,
+                wait_for_ready=client_call_details.wait_for_ready,
+            )
+        else:
+            # Default assuming previous interceptors have passed proper `Metadata()` object for now and just copy over
+            new_client_call_details = ClientCallDetails(
+                method=client_call_details.method,
+                timeout=client_call_details.timeout,
+                metadata=client_call_details.metadata,
+                credentials=client_call_details.credentials,
+                wait_for_ready=client_call_details.wait_for_ready,
+            )
         for header in self.headers_to_add_every_time:
-            client_call_details.metadata.add(header.name, header.value)
+            new_client_call_details.metadata.add(header.name, header.value)
         if not AddHeaderClientInterceptor.are_only_once_headers_sent:
             for header in self._headers_to_add_once:
-                client_call_details.metadata.add(header.name, header.value)
+                new_client_call_details.metadata.add(header.name, header.value)
             AddHeaderClientInterceptor.are_only_once_headers_sent = True
 
-        return await continuation(client_call_details, request)
+        return await continuation(new_client_call_details, request)
