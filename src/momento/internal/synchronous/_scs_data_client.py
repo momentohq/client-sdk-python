6716007785db00cdf5d23103f2091ff6aa53ae0e
Chris Price <chris@momentohq.com>
2022-12-16 20:29:29 -0800

    feat: migrate synchronous API to consume gRPC synchronous API
    Prior to this commit, our synchronous API was implemented as a
    wrapper around our asynchronous API, with the intention of
    reducing code duplication and making it easier to maintain
    both.
    
    However, after deeper investigation of how that
    approach to the synchronous API implementation behaves in
    applications that are using asyncio for other purposes,
    we have decided that it will be safer and a better user
    experience to implement it such that it directly consumes
    the gRPC synchronous API.
    
    This may require a bit more code and maintenance, but it
    will give us the best UX for users who need to consume
    our synchronous API in an app that uses other asyncio
    libraries.

---
+++ src/momento/internal/synchronous/_scs_data_client.py
@@ -0,0 +1,107 @@
+from typing import Union, Optional, List, Tuple
+
+from momento_wire_types.cacheclient_pb2 import _GetRequest, _GetResponse
+from momento_wire_types.cacheclient_pb2 import _SetRequest, _SetResponse
+from momento_wire_types.cacheclient_pb2 import _DeleteRequest, _DeleteResponse
+from momento_wire_types.cacheclient_pb2_grpc import ScsStub
+
+from momento import cache_operation_types
+from . import _scs_grpc_manager
+
+from momento._utilities._data_validation import (
+    _validate_ttl
+)
+from ..common._data_client_ops import construct_set_response, construct_get_response, \
+    construct_delete_response, wrap_with_error_handling, prepare_get_request, prepare_set_request, \
+    prepare_delete_request
+
+_DEFAULT_DEADLINE_SECONDS = 5.0  # 5 seconds
+
+
+def _make_metadata(cache_name: str) -> List[Tuple[str, str]]:
+    return [("cache", cache_name)]
+
+
+class _ScsDataClient:
+    """Internal"""
+
+    def __init__(
+        self,
+        auth_token: str,
+        endpoint: str,
+        default_ttl_seconds: int,
+        request_timeout_ms: Optional[int],
+    ):
+        self._default_deadline_seconds = (
+            _DEFAULT_DEADLINE_SECONDS
+            if not request_timeout_ms
+            else request_timeout_ms / 1000.0
+        )
+        self._grpc_manager = _scs_grpc_manager._DataGrpcManager(auth_token, endpoint)
+        _validate_ttl(default_ttl_seconds)
+        self._default_ttlSeconds = default_ttl_seconds
+
+    def set(
+        self,
+        cache_name: str,
+        key: str,
+        value: Union[str, bytes],
+        ttl_seconds: Optional[int],
+    ) -> cache_operation_types.CacheSetResponse:
+        def execute_set_request_fn(req: _SetRequest) -> _SetResponse:
+            return self._getStub().Set(
+                req,
+                metadata=_make_metadata(cache_name),
+                timeout=self._default_deadline_seconds,
+            )
+
+        return wrap_with_error_handling(
+            cache_name=cache_name,
+            request_type='Set',
+            prepare_request_fn=lambda: prepare_set_request(
+                key=key,
+                value=value,
+                ttl_seconds=ttl_seconds,
+                default_ttl_seconds=self._default_ttlSeconds
+            ),
+            execute_request_fn=execute_set_request_fn,
+            response_fn=construct_set_response
+        )
+
+    def get(self, cache_name: str, key: Union[str, bytes]) -> cache_operation_types.CacheGetResponse:
+        def execute_get_request_fn(req: _GetRequest) -> _GetResponse:
+            return self._getStub().Get(
+                req,
+                metadata=_make_metadata(cache_name),
+                timeout=self._default_deadline_seconds,
+            )
+
+        return wrap_with_error_handling(
+            cache_name=cache_name,
+            request_type='Get',
+            prepare_request_fn=lambda: prepare_get_request(key),
+            execute_request_fn=execute_get_request_fn,
+            response_fn=construct_get_response
+        )
+
+    def delete(self, cache_name: str, key: str) -> cache_operation_types.CacheDeleteResponse:
+        def execute_delete_request_fn(req: _DeleteRequest) -> _DeleteResponse:
+            return self._getStub().Delete(
+                req,
+                metadata=_make_metadata(cache_name),
+                timeout=self._default_deadline_seconds,
+            )
+
+        return wrap_with_error_handling(
+            cache_name=cache_name,
+            request_type='Delete',
+            prepare_request_fn=lambda: prepare_delete_request(key),
+            execute_request_fn=execute_delete_request_fn,
+            response_fn=construct_delete_response
+        )
+
+    def _getStub(self) -> ScsStub:
+        return self._grpc_manager.stub()
+
+    def close(self) -> None:
+        self._grpc_manager.close()
