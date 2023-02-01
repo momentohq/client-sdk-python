a200f21ba3eca60bedb54dfe4e1cd5a92bbd0d69
Michael Landis <michael@momentohq.com>
2023-02-01 08:57:04 -0800

    chore: update examples to use latest python client (#197)
    We update the momento dependency to 0.19.0 and update the examples as
    appropriate. Because pattern matching is available for 3.10+, we
    provide examples with and without pattern matching.
    
    Hence we now have examples for {async, sync} x {pattern matching, legacy-if-else}

---
+++ examples/py310/example.py
@@ -0,0 +1,94 @@
+import logging
+from datetime import timedelta
+
+from example_utils.example_logging import initialize_logging
+
+from momento import SimpleCacheClient
+from momento.auth.credential_provider import EnvMomentoTokenProvider
+from momento.config.configurations import Laptop
+from momento.responses import CacheGet, CacheSet, CreateCache, ListCaches
+
+_AUTH_PROVIDER = EnvMomentoTokenProvider("MOMENTO_AUTH_TOKEN")
+_CACHE_NAME = "cache"
+_ITEM_DEFAULT_TTL_SECONDS = timedelta(seconds=60)
+_KEY = "MyKey"
+_VALUE = "MyValue"
+
+_logger = logging.getLogger("momento-example")
+
+
+def _print_start_banner() -> None:
+    _logger.info("******************************************************************")
+    _logger.info("*                      Momento Example Start                     *")
+    _logger.info("******************************************************************")
+
+
+def _print_end_banner() -> None:
+    _logger.info("******************************************************************")
+    _logger.info("*                       Momento Example End                      *")
+    _logger.info("******************************************************************")
+
+
+def _create_cache(cache_client: SimpleCacheClient, cache_name: str) -> None:
+    create_cache_response = cache_client.create_cache(cache_name)
+    match create_cache_response:
+        case CreateCache.Success():
+            pass
+        case CreateCache.CacheAlreadyExists():
+            _logger.info(f"Cache with name: {cache_name!r} already exists.")
+        case CreateCache.Error() as error:
+            _logger.error(f"Error creating cache: {error.message}")
+        case _:
+            _logger.error("Unreachable")
+
+
+def _list_caches(cache_client: SimpleCacheClient) -> None:
+    _logger.info("Listing caches:")
+    list_caches_response = cache_client.list_caches()
+    while True:
+        match list_caches_response:
+            case ListCaches.Success() as success:
+                for cache_info in success.caches:
+                    _logger.info(f"- {cache_info.name!r}")
+                next_token = success.next_token
+                if next_token is None:
+                    break
+            case ListCaches.Error() as error:
+                _logger.error(f"Error creating cache: {error.message}")
+            case _:
+                _logger.error("Unreachable")
+
+        list_caches_response = cache_client.list_caches(next_token)
+    _logger.info("")
+
+
+if __name__ == "__main__":
+    initialize_logging()
+    _print_start_banner()
+    with SimpleCacheClient(Laptop.latest(), _AUTH_PROVIDER, _ITEM_DEFAULT_TTL_SECONDS) as cache_client:
+        _create_cache(cache_client, _CACHE_NAME)
+        _list_caches(cache_client)
+
+        _logger.info(f"Setting Key: {_KEY!r} Value: {_VALUE!r}")
+        set_response = cache_client.set(_CACHE_NAME, _KEY, _VALUE)
+        match set_response:
+            case CacheSet.Success():
+                pass
+            case CacheSet.Error() as error:
+                _logger.error(f"Error creating cache: {error.message}")
+            case _:
+                _logger.error("Unreachable")
+
+        _logger.info(f"Getting Key: {_KEY!r}")
+        get_response = cache_client.get(_CACHE_NAME, _KEY)
+        match get_response:
+            case CacheGet.Hit() as hit:
+                _logger.info(f"Look up resulted in a hit: {hit}")
+                _logger.info(f"Looked up Value: {hit.value_string!r}")
+            case CacheGet.Miss():
+                _logger.info("Look up resulted in a: miss. This is unexpected.")
+            case CacheGet.Error() as error:
+                _logger.error(f"Error creating cache: {error.message}")
+            case _:
+                _logger.error("Unreachable")
+    _print_end_banner()
---
+++ examples/py310/example_async.py
@@ -0,0 +1,99 @@
+import asyncio
+import logging
+from datetime import timedelta
+
+from example_utils.example_logging import initialize_logging
+
+from momento import SimpleCacheClientAsync
+from momento.auth.credential_provider import EnvMomentoTokenProvider
+from momento.config.configurations import Laptop
+from momento.responses import CacheGet, CacheSet, CreateCache, ListCaches
+
+_AUTH_PROVIDER = EnvMomentoTokenProvider("MOMENTO_AUTH_TOKEN")
+_CACHE_NAME = "cache"
+_ITEM_DEFAULT_TTL_SECONDS = timedelta(seconds=60)
+_KEY = "MyKey"
+_VALUE = "MyValue"
+
+_logger = logging.getLogger("momento-example")
+
+
+def _print_start_banner() -> None:
+    _logger.info("******************************************************************")
+    _logger.info("*                      Momento Example Start                     *")
+    _logger.info("******************************************************************")
+
+
+def _print_end_banner() -> None:
+    _logger.info("******************************************************************")
+    _logger.info("*                       Momento Example End                      *")
+    _logger.info("******************************************************************")
+
+
+async def _create_cache(cache_client: SimpleCacheClientAsync, cache_name: str) -> None:
+    create_cache_response = await cache_client.create_cache(cache_name)
+    match create_cache_response:
+        case CreateCache.Success():
+            pass
+        case CreateCache.CacheAlreadyExists():
+            _logger.info(f"Cache with name: {cache_name!r} already exists.")
+        case CreateCache.Error() as error:
+            _logger.error(f"Error creating cache: {error.message}")
+        case _:
+            _logger.error("Unreachable")
+
+
+async def _list_caches(cache_client: SimpleCacheClientAsync) -> None:
+    _logger.info("Listing caches:")
+    list_caches_response = await cache_client.list_caches()
+    while True:
+        match list_caches_response:
+            case ListCaches.Success() as success:
+                for cache_info in success.caches:
+                    _logger.info(f"- {cache_info.name!r}")
+                next_token = success.next_token
+                if next_token is None:
+                    break
+            case ListCaches.Error() as error:
+                _logger.error(f"Error creating cache: {error.message}")
+            case _:
+                _logger.error("Unreachable")
+
+        list_caches_response = await cache_client.list_caches(next_token)
+    _logger.info("")
+
+
+async def main() -> None:
+    initialize_logging()
+    _print_start_banner()
+    async with SimpleCacheClientAsync(Laptop.latest(), _AUTH_PROVIDER, _ITEM_DEFAULT_TTL_SECONDS) as cache_client:
+        await _create_cache(cache_client, _CACHE_NAME)
+        await _list_caches(cache_client)
+
+        _logger.info(f"Setting Key: {_KEY!r} Value: {_VALUE!r}")
+        set_response = await cache_client.set(_CACHE_NAME, _KEY, _VALUE)
+        match set_response:
+            case CacheSet.Success():
+                pass
+            case CacheSet.Error() as error:
+                _logger.error(f"Error creating cache: {error.message}")
+            case _:
+                _logger.error("Unreachable")
+
+        _logger.info(f"Getting Key: {_KEY!r}")
+        get_response = await cache_client.get(_CACHE_NAME, _KEY)
+        match get_response:
+            case CacheGet.Hit() as hit:
+                _logger.info(f"Look up resulted in a hit: {hit}")
+                _logger.info(f"Looked up Value: {hit.value_string!r}")
+            case CacheGet.Miss():
+                _logger.info("Look up resulted in a: miss. This is unexpected.")
+            case CacheGet.Error() as error:
+                _logger.error(f"Error creating cache: {error.message}")
+            case _:
+                _logger.error("Unreachable")
+    _print_end_banner()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
