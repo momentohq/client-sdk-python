9e9a95ac63ff16d81c067ef2a9d29908e69977e7
Michael Landis <michael@momentohq.com>
2023-02-09 21:37:02 -0800

    chore: truncate string representation but not repr
    See https://docs.python.org/2/reference/datamodel.html#object.__repr__
    for a discussion of `__repr__` vs `__str__`. The former, as used for
    debugging, should be as close to unaltered as possible.

--- src/momento/responses/response.py
+++ src/momento/responses/response.py
@@ -9,16 +9,26 @@ class Response(ABC):
 
     # We need to work with Any.
     @no_type_check
-    def __repr__(self) -> str:
+    def _render_as_str(self, truncate: bool = False) -> str:
         class_name = self.__class__.__qualname__
         attributes = vars(self)
 
         if not attributes:
             return f"{class_name}()"
 
-        message = ", ".join(f"{k}={self._truncate_value(v)!r}" for k, v in attributes.items())
+        message = ", ".join(
+            f"{k}={self._truncate_value(v)!r}" if truncate else f"{k}={v!r}" for k, v in attributes.items()
+        )
 
         return f"{class_name}({message})"
+
+    @no_type_check
+    def __repr__(self) -> str:
+        return self._render_as_str()
+
+    @no_type_check
+    def __str__(self) -> str:
+        return self._render_as_str(truncate=True)
 
     @no_type_check
     def _truncate_value(self, value: Any, max_length: int = 32) -> Any:
