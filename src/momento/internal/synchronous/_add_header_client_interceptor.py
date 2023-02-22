3e2cc2ad59f486e56958509258e0ba4b2ca8f90f
Michael Landis <michael@momentohq.com>
2023-02-22 11:29:30 -0800

    chore: extract remark from docstring to comment

--- src/momento/internal/synchronous/_add_header_client_interceptor.py
+++ src/momento/internal/synchronous/_add_header_client_interceptor.py
@@ -63,16 +63,16 @@ class AddHeaderClientInterceptor(grpc.UnaryUnaryClientInterceptor):
 def sanitize_client_call_details(client_call_details: grpc.ClientCallDetails) -> grpc.ClientCallDetails:
     """Defensive function meant to handle inbound gRPC client request objects.
 
-    Makes sure we can handle properly when we inject our own metadata onto request object.
-    This was mainly done as temporary fix after we observed ddtrace grpc client interceptor passing
-    client_call_details.metadata as a list instead of a grpc.aio.Metadata object.
-    See this ticket for follow-up actions to come back in and address this longer term:
-        https://github.com/momentohq/client-sdk-python/issues/149
     Args:
         client_call_details: the original inbound client grpc request we are intercepting
 
     Returns: a new client_call_details object with metadata properly initialized to a `grpc.aio.Metadata` object
     """
+    # Makes sure we can handle properly when we inject our own metadata onto request object.
+    # This was mainly done as temporary fix after we observed ddtrace grpc client interceptor passing
+    # client_call_details.metadata as a list instead of a grpc.aio.Metadata object.
+    # See this ticket for follow-up actions to come back in and address this longer term:
+    #    https://github.com/momentohq/client-sdk-python/issues/149
     # If no metadata set on passed in client call details then we are first to set, so we should just initialize
     if client_call_details.metadata is None:
         return _ClientCallDetails(
