73afb3c6da8f3e1baaf400dab06abfbd7f7037ae
Pete Gautier <pete@momentohq.com>
2023-06-01 10:12:56 -0700

    chore: add py310 and prepy310 examples

---
+++ examples/py310/readme.py
@@ -0,0 +1,22 @@
+import logging
+from datetime import timedelta
+
+from example_utils.example_logging import initialize_logging
+
+from momento import CacheClient, Configurations, CredentialProvider
+from momento.responses import CacheGet, CacheSet, CreateCache, ListCaches
+
+cache_client = CacheClient(
+    Configurations.Laptop.v1(),
+    CredentialProvider.from_environment_variable("MOMENTO_AUTH_TOKEN"),
+    timedelta(seconds=60)
+)
+
+create_cache_response = cache_client.create_cache("cache")
+set_response = cache_client.set("cache", "my-key", "my-value")
+get_response = cache_client.get(_CACHE_NAME, _KEY)
+match get_response:
+    case CacheGet.Hit() as hit:
+        print(f"Got value: {hit.value_string}")
+    case _:
+        print(f"Response was not a hit: {get_response}")
