d7a09e4219b73f081d752548cf7afbb385f98ca3
rishtigupta <rishti@momentohq.com>
2023-06-05 09:40:01 -0700

    feat: add traces with open telemetry

---
+++ examples/observability.py
@@ -0,0 +1,62 @@
+from utils.instrumentation import example_observability_setup_tracing
+
+example_observability_setup_tracing()
+
+from momento import CacheClient, Configurations, CredentialProvider
+from momento.responses import CreateCache, CacheSet, CacheGet
+from datetime import timedelta
+import logging
+
+_AUTH_PROVIDER = CredentialProvider.from_environment_variable("MOMENTO_AUTH_TOKEN")
+_ITEM_DEFAULT_TTL_SECONDS = timedelta(seconds=60)
+_CACHE_NAME = "test-cache"
+_KEY = "test-key"
+_VALUE = "test-value"
+_logger = logging.getLogger("momento-example")
+
+
+def _create_cache(cache_client: CacheClient) -> None:
+    create_cache_response = cache_client.create_cache(_CACHE_NAME)
+    match create_cache_response:
+        case CreateCache.Success():
+            pass
+        case CreateCache.CacheAlreadyExists():
+            print(f"Cache with name: {_CACHE_NAME!r} already exists.")
+        case CreateCache.Error() as error:
+            print(f"Error creating cache: {error.message}")
+        case _:
+            print("Unreachable")
+
+
+def _set_cache(cache_client: CacheClient) -> None:
+    set_cache_response = cache_client.set(_CACHE_NAME, _KEY, _VALUE)
+    match set_cache_response:
+        case CacheSet.Success():
+            pass
+        case CacheSet.Error() as error:
+            print(f"Error setting value: {error.message}")
+        case _:
+            print("Unreachable")
+
+
+def _get_cache(cache_client: CacheClient) -> None:
+    get_cache_response = cache_client.get(_CACHE_NAME, _KEY)
+    match get_cache_response:
+        case CacheGet.Hit():
+            print("Value is " + get_cache_response.value_string)
+            pass
+        case CacheGet.Error() as error:
+            print(f"Error setting value: {error.message}")
+        case _:
+            print("Unreachable")
+
+
+def main() -> None:
+    with CacheClient(Configurations.Laptop.v1(), _AUTH_PROVIDER, _ITEM_DEFAULT_TTL_SECONDS) as cache_client:
+        _create_cache(cache_client)
+        _set_cache(cache_client)
+        _get_cache(cache_client)
+
+
+main()
+print('Success! Zipkin at http://localhost:9411 should contain traces for the cache creation, get, and set.')
---
+++ examples/utils/instrumentation.py
@@ -0,0 +1,29 @@
+from opentelemetry import trace
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.resources import Resource, SERVICE_NAME
+from opentelemetry.sdk.trace.export import SimpleSpanProcessor
+from opentelemetry.exporter.zipkin.json import ZipkinExporter
+from opentelemetry.instrumentation.grpc import GrpcInstrumentorClient
+
+
+def example_observability_setup_tracing():
+    # Create a resource object
+    resource = Resource(attributes={
+        SERVICE_NAME: "momento_requests_counter"
+    })
+
+    # Create a tracer provider
+    tracer_provider = TracerProvider(resource=resource)
+
+    # Create a Zipkin exporter
+    zipkin_exporter = ZipkinExporter()
+
+    # Create a span processor and add the exporter
+    span_processor = SimpleSpanProcessor(zipkin_exporter)
+    tracer_provider.add_span_processor(span_processor)
+
+    # Register the tracer provider
+    trace.set_tracer_provider(tracer_provider)
+
+    # Register the gRPC instrumentation
+    GrpcInstrumentorClient().instrument()
