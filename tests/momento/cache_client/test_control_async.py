3bad96f4796bd03ddbef839f0096da6a492b6a7f
Pratik Agarwal <pratik@momentohq.com>
2023-08-29 15:49:50 -0300

    chore: remove signng key test

--- tests/momento/cache_client/test_control_async.py
+++ tests/momento/cache_client/test_control_async.py
@@ -216,17 +216,17 @@ async def test_list_caches_succeeds_even_if_cred_provider_has_been_printed() -> 
         assert isinstance(response, ListCaches.Success)
 
 
-async def test_create_list_revoke_signing_keys(client_async: CacheClientAsync) -> None:
-    create_response = await client_async.create_signing_key(timedelta(minutes=30))
-    assert isinstance(create_response, CreateSigningKey.Success)
-
-    list_response = await client_async.list_signing_keys()
-    assert isinstance(list_response, ListSigningKeys.Success)
-    assert create_response.key_id in [signing_key.key_id for signing_key in list_response.signing_keys]
-
-    revoke_response = await client_async.revoke_signing_key(create_response.key_id)
-    assert isinstance(revoke_response, RevokeSigningKey.Success)
-
-    list_response = await client_async.list_signing_keys()
-    assert isinstance(list_response, ListSigningKeys.Success)
-    assert create_response.key_id not in [signing_key.key_id for signing_key in list_response.signing_keys]
+# async def test_create_list_revoke_signing_keys(client_async: CacheClientAsync) -> None:
+#     create_response = await client_async.create_signing_key(timedelta(minutes=30))
+#     assert isinstance(create_response, CreateSigningKey.Success)
+#
+#     list_response = await client_async.list_signing_keys()
+#     assert isinstance(list_response, ListSigningKeys.Success)
+#     assert create_response.key_id in [signing_key.key_id for signing_key in list_response.signing_keys]
+#
+#     revoke_response = await client_async.revoke_signing_key(create_response.key_id)
+#     assert isinstance(revoke_response, RevokeSigningKey.Success)
+#
+#     list_response = await client_async.list_signing_keys()
+#     assert isinstance(list_response, ListSigningKeys.Success)
+#     assert create_response.key_id not in [signing_key.key_id for signing_key in list_response.signing_keys]
