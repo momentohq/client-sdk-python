d3cc08c830af312ffe45d2dd8d5ef8bada079739
Pratik Agarwal <pratik@momentohq.com>
2023-08-30 10:01:12 -0300

    chore: remove bad token

--- examples/repro5xx/repro.py
+++ examples/repro5xx/repro.py
@@ -1,29 +1,21 @@
-import os
-import random
 from datetime import timedelta
 
 from momento import CacheClient, Configurations, CredentialProvider
 from momento.responses import CacheGet, CacheSet, CreateCache, ListCaches
 
-BAD_AUTH_TOKEN: str = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJpbnRlZ3JhdGlvbiIsImNwIjoiY29udHJvbC5jZWxsLWFscGhhLWRldi5wcmVwcm9kLmEubW9tZW50b2hxLmNvbSIsImMiOiJjYWNoZS5jZWxsLWFscGhhLWRldi5wcmVwcm9kLmEubW9tZW50b2hxLmNvbSJ9.gdghdjjfjyehhdkkkskskmmls76573jnajhjjjhjdhnndy"
-
 def do_work():
-    os.environ["BAD_AUTH_TOKEN"] = BAD_AUTH_TOKEN
-    print("Creating client")
-    bad_client = CacheClient.create(Configurations.Laptop.latest(), CredentialProvider.from_environment_variable("BAD_AUTH_TOKEN"),
-                       timedelta(seconds=60))
-    print(bad_client.list_caches())
     clients = []
-    for i in range(0, 10):
+    for i in range(0, 5):
         clients.append(CacheClient.create(Configurations.Laptop.latest(), CredentialProvider.from_environment_variable("MOMENTO_AUTH_TOKEN"),
                          timedelta(seconds=60)))
         print("Created client " + str(i))
 
     for i in range(0, 1000):
-        client = clients[i % 10]
-        print(client.create_cache(str(i)))
+        client = clients[i % 5]
+        create = client.create_cache(str(i))
         print(client.list_caches())
-        print(client.delete_cache(str(i)))
+        if isinstance(create, CreateCache.Success):
+            print(client.delete_cache(str(i)))
 
 
 do_work()
