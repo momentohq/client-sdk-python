8904122397a01464b74b2546fbbf21a3644c35b6
Michael Landis <michael@momentohq.com>
2023-10-19 19:22:04 -0700

    feat: support storing and retrieving diverse metadata types
    In the previous version of the service, we only supported
    string-valued metadata. In the latest version, we support the
    following value types:
    
    - string
    - int
    - float
    - bool
    - list of strings
    
    We update the `Item` class to support serializing these types and the
    `SearchHit` class to deserialize.

--- src/momento/requests/vector_index/item.py
+++ src/momento/requests/vector_index/item.py
@@ -19,7 +19,7 @@ class Item:
     vector: list[float]
     """The vector of the item."""
 
-    metadata: dict[str, str] = field(default_factory=dict)
+    metadata: dict[str, str | int | float | bool | list[str]] = field(default_factory=dict)
     """The metadata of the item."""
 
     def to_proto(self) -> pb._Item:
@@ -27,11 +27,21 @@ class Item:
         metadata = []
         if self.metadata is not None:
             for k, v in self.metadata.items():
-                if type(v) is not str:
+                if type(v) == str:
+                    metadata.append(pb._Metadata(field=k, string_value=v))
+                elif type(v) == int:
+                    metadata.append(pb._Metadata(field=k, integer_value=v))
+                elif type(v) == float:
+                    metadata.append(pb._Metadata(field=k, double_value=v))
+                elif type(v) == bool:
+                    metadata.append(pb._Metadata(field=k, boolean_value=v))
+                elif type(v) == list and all(type(x) == str for x in v):
+                    list_of_strings = pb._Metadata._ListOfStrings(values=v)
+                    metadata.append(pb._Metadata(field=k, list_of_strings_value=list_of_strings))
+                else:
                     raise InvalidArgumentException(
-                        f"Metadata values must be strings. Field {k!r} has a value of type {type(v)!r} with value {v!r}.",  # noqa: E501
+                        f"Metadata values must be either str, int, float, bool, or list[str]. Field {k!r} has a value of type {type(v)!r} with value {v!r}.",  # noqa: E501
                         Service.INDEX,
                     )
-                metadata.append(pb._Metadata(field=k, string_value=v))
 
         return pb._Item(id=self.id, vector=vector, metadata=metadata)
