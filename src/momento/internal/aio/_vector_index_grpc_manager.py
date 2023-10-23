3a2c7f0df1e38fbce3d007d60d0ca18a731df1a3
Michael Landis <michael@momentohq.com>
2023-10-23 15:38:18 -0700

    feat: allow custom root certificates in vector index client config (#394)
    Adds a with_root_certificates_pem to read a certificate pem
    file. Exposes a with_root_certificates_pem method to the top level
    of VectorIndexConfiguration for convenience. The clients are
    instantiated with either the default ssl channel creds or ones using
    the root certificates, if available.

--- src/momento/internal/aio/_vector_index_grpc_manager.py
+++ src/momento/internal/aio/_vector_index_grpc_manager.py
@@ -7,6 +7,9 @@ from momento_wire_types import vectorindex_pb2_grpc as vector_index_client
 from momento.auth import CredentialProvider
 from momento.config import VectorIndexConfiguration
 from momento.internal._utilities import momento_version
+from momento.internal._utilities._channel_credentials import (
+    vector_credentials_from_root_certs_or_default,
+)
 
 from ._add_header_client_interceptor import AddHeaderClientInterceptor, Header
 
@@ -19,7 +22,7 @@ class _VectorIndexControlGrpcManager:
     def __init__(self, configuration: VectorIndexConfiguration, credential_provider: CredentialProvider):
         self._secure_channel = grpc.aio.secure_channel(
             target=credential_provider.control_endpoint,
-            credentials=grpc.ssl_channel_credentials(),
+            credentials=vector_credentials_from_root_certs_or_default(configuration),
             interceptors=_interceptors(credential_provider.auth_token),
         )
 
@@ -38,7 +41,7 @@ class _VectorIndexDataGrpcManager:
     def __init__(self, configuration: VectorIndexConfiguration, credential_provider: CredentialProvider):
         self._secure_channel = grpc.aio.secure_channel(
             target=credential_provider.vector_endpoint,
-            credentials=grpc.ssl_channel_credentials(),
+            credentials=vector_credentials_from_root_certs_or_default(configuration),
             interceptors=_interceptors(credential_provider.auth_token),
         )
 
