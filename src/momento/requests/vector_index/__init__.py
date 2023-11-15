571ac2ac26c8cc8d7361344cd3b83673da8e8101
Michael Landis <michael@momentohq.com>
2023-11-14 17:44:59 -0800

    feat: deserialize more list index detail

--- src/momento/requests/vector_index/__init__.py
+++ src/momento/requests/vector_index/__init__.py
@@ -1,5 +1,5 @@
-from .create_index import SimilarityMetric
 from .item import Item
 from .search import ALL_METADATA, AllMetadata
+from .similarity_metric import SimilarityMetric
 
 __all__ = ["Item", "AllMetadata", "ALL_METADATA", "SimilarityMetric"]
