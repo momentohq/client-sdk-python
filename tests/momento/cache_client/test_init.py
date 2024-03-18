1da7eb9bd78da2e2e87665998d464c59ed1441ad
anitarua <anita@momentohq.com>
2024-03-18 13:58:58 -0700

    add some tests

--- tests/momento/cache_client/test_init.py
+++ tests/momento/cache_client/test_init.py
@@ -43,3 +43,12 @@ def test_init_throws_exception_when_client_uses_zero_request_timeout_ms(
     with pytest.raises(InvalidArgumentException, match="Request timeout must be a positive amount of time."):
         configuration = configuration.with_client_timeout(timedelta(seconds=0))
         CacheClient(configuration, credential_provider, default_ttl_seconds)
+
+
+def test_init_eagerly_connecting_cache_client(
+    configuration: Configuration, credential_provider: CredentialProvider, default_ttl_seconds: timedelta
+) -> None:
+    client = CacheClient.create(
+        configuration, credential_provider, default_ttl_seconds, eager_connection_timeout=timedelta(seconds=30)
+    )
+    assert client
