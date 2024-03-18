3c97c4061c25406c1252a4f1958cea373a5471e4
Anita Ruangrotsakun <138700973+anitarua@users.noreply.github.com>
2024-03-18 16:30:00 -0700

    chore: eager connection should fail fast (#445)
    * chore: eager connection should fail fast
    
    * add some tests

--- tests/momento/cache_client/test_init_async.py
+++ tests/momento/cache_client/test_init_async.py
@@ -45,3 +45,12 @@ async def test_init_throws_exception_when_client_uses_zero_request_timeout_ms(
     with pytest.raises(InvalidArgumentException, match="Request timeout must be a positive amount of time."):
         configuration = configuration.with_client_timeout(timedelta(seconds=0))
         CacheClientAsync(configuration, credential_provider, default_ttl_seconds)
+
+
+async def test_init_eagerly_connecting_cache_client(
+    configuration: Configuration, credential_provider: CredentialProvider, default_ttl_seconds: timedelta
+) -> None:
+    client = await CacheClientAsync.create(
+        configuration, credential_provider, default_ttl_seconds, eager_connection_timeout=timedelta(seconds=30)
+    )
+    assert client
