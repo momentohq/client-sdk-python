848126cd10151d95328b7be55b9cc6a0c63c3ce1
Michael Landis <michael@momentohq.com>
2024-09-19 11:47:49 -0700

    chore: fall back manual version when dist-info absent
    We temporarily fall back to a manually provided version number when
    the `dist-info` METADATA is absent. This situation occurs in some
    frameworks like `serverless` which strip this for a slim build.
    
    In the near future we will refactor this code so that CI sets the
    version number in the file and we do not rely on metadata reflection.

--- src/momento/internal/_utilities/_momento_version.py
+++ src/momento/internal/_utilities/_momento_version.py
@@ -4,15 +4,21 @@ This module is used to detect the version of momento installed. It is used
 internally to add the version to the gRPC headers.
 """
 
+from importlib.metadata import PackageNotFoundError
+
 momento_version = ""
 
 try:
-    # For python < 3.8
-    import importlib_metadata
+    try:
+        # For python < 3.8
+        import importlib_metadata
 
-    momento_version = importlib_metadata.Distribution.from_name("momento").version  # type: ignore[no-untyped-call,misc]
-except (ImportError, ModuleNotFoundError):
-    # For python >= 3.8
-    from importlib.metadata import version  # type: ignore[import]
+        momento_version = importlib_metadata.Distribution.from_name("momento").version  # type: ignore[no-untyped-call,misc]
+    except ImportError:
+        # For python >= 3.8
+        from importlib.metadata import version  # type: ignore[import]
 
-    momento_version = version("momento")
+        momento_version = version("momento")
+except (ImportError, ModuleNotFoundError, PackageNotFoundError):
+    # Fall back to setting to the version manually
+    momento_version = "1.21.2"
