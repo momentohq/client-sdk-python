06cceab0ff111cb6c992676fe3de2cdc8a43c98f
Pete Gautier <pete@momentohq.com>
2024-11-19 16:25:02 -0800

    chore: add tests

--- tests/momento/topic_client/test_topics.py
+++ tests/momento/topic_client/test_topics.py
@@ -49,20 +49,59 @@ def describe_subscribe() -> None:
         cache_name = uuid_str()
         return partial(topic_client.subscribe, cache_name=cache_name)
 
-    def subscribe_happy_path_string(client: CacheClient, topic_client: TopicClient, cache_name: str) -> None:
+    def subscribe_happy_path_string(topic_client: TopicClient, cache_name: str) -> None:
         topic = uuid_str()
         value = uuid_str()
+
+        _ = topic_client.publish(cache_name, topic_name=topic, value=value)
 
         subscribe_response = topic_client.subscribe(cache_name, topic_name=topic)
         assert isinstance(subscribe_response, TopicSubscribe.Subscription)
 
-        _ = topic_client.publish(cache_name, topic_name=topic, value=value)
-
         item_response = next(subscribe_response)
+
         assert isinstance(item_response, TopicSubscriptionItem.Text)
         assert item_response.value == value
 
-    def subscribe_happy_path_binary(client: CacheClient, topic_client: TopicClient, cache_name: str) -> None:
+    def subscribe_happy_path_string_with_nonzero_resume(
+        topic_client: TopicClient, cache_name: str
+    ) -> None:
+        topic = uuid_str()
+        value = uuid_str()
+
+        _ = topic_client.publish(cache_name, topic_name=topic, value="1")
+        _ = topic_client.publish(cache_name, topic_name=topic, value="2")
+        _ = topic_client.publish(cache_name, topic_name=topic, value=value)
+
+        subscribe_response = topic_client.subscribe(
+            cache_name, topic_name=topic, resume_at_topic_sequence_number=3, resume_at_topic_sequence_page=0
+        )
+        assert isinstance(subscribe_response, TopicSubscribe.Subscription)
+
+        item_response = next(subscribe_response)
+
+        assert isinstance(item_response, TopicSubscriptionItem.Text)
+        assert item_response.value == value
+
+    def subscribe_happy_path_string_with_discontinuity(
+        topic_client: TopicClient, cache_name: str
+    ) -> None:
+        topic = uuid_str()
+        value = uuid_str()
+
+        _ = topic_client.publish(cache_name, topic_name=topic, value=value)
+
+        subscribe_response = topic_client.subscribe(
+            cache_name, topic_name=topic, resume_at_topic_sequence_number=5, resume_at_topic_sequence_page=5
+        )
+        assert isinstance(subscribe_response, TopicSubscribe.Subscription)
+
+        item_response = next(subscribe_response)
+
+        assert isinstance(item_response, TopicSubscriptionItem.Text)
+        assert item_response.value == value
+
+    def subscribe_happy_path_binary(topic_client: TopicClient, cache_name: str) -> None:
         topic = uuid_str()
         value = uuid_bytes()
 
@@ -75,7 +114,7 @@ def describe_subscribe() -> None:
         assert isinstance(item_response, TopicSubscriptionItem.Binary)
         assert item_response.value == value
 
-    def succeeds_with_nonexistent_topic(client: CacheClient, topic_client: TopicClient, cache_name: str) -> None:
+    def succeeds_with_nonexistent_topic(topic_client: TopicClient, cache_name: str) -> None:
         topic = uuid_str()
 
         resp = topic_client.subscribe(cache_name, topic)
--- tests/momento/topic_client/test_topics_async.py
+++ tests/momento/topic_client/test_topics_async.py
@@ -30,7 +30,7 @@ def describe_publish() -> None:
         return partial(topic_client_async.publish, cache_name=cache_name, value=value)
 
     async def publish_happy_path(
-        client: CacheClientAsync, topic_client_async: TopicClientAsync, cache_name: str
+        topic_client_async: TopicClientAsync, cache_name: str
     ) -> None:
         topic = uuid_str()
         value = uuid_str()
@@ -52,24 +52,61 @@ def describe_subscribe() -> None:
         return partial(topic_client_async.subscribe, cache_name=cache_name)
 
     async def subscribe_happy_path_string(
-        client: CacheClientAsync, topic_client_async: TopicClientAsync, cache_name: str
+        topic_client_async: TopicClientAsync, cache_name: str
     ) -> None:
         topic = uuid_str()
         value = uuid_str()
+
+        _ = await topic_client_async.publish(cache_name, topic_name=topic, value=value)
 
         subscribe_response = await topic_client_async.subscribe(cache_name, topic_name=topic)
         assert isinstance(subscribe_response, TopicSubscribe.SubscriptionAsync)
 
         item_task = subscribe_response.__anext__()
-        publish_response = await topic_client_async.publish(cache_name, topic_name=topic, value=value)
+        item_response = await item_task
+        assert isinstance(item_response, TopicSubscriptionItem.Text)
+        assert item_response.value == value
 
-        print(publish_response)
+    async def subscribe_happy_path_string_with_nonzero_resume(
+        topic_client_async: TopicClientAsync, cache_name: str
+    ) -> None:
+        topic = uuid_str()
+        value = uuid_str()
+
+        _ = await topic_client_async.publish(cache_name, topic_name=topic, value="1")
+        _ = await topic_client_async.publish(cache_name, topic_name=topic, value="2")
+        _ = await topic_client_async.publish(cache_name, topic_name=topic, value=value)
+
+        subscribe_response = await topic_client_async.subscribe(
+            cache_name, topic_name=topic, resume_at_topic_sequence_number=3, resume_at_topic_sequence_page=0
+        )
+        assert isinstance(subscribe_response, TopicSubscribe.SubscriptionAsync)
+
+        item_task = subscribe_response.__anext__()
+        item_response = await item_task
+        assert isinstance(item_response, TopicSubscriptionItem.Text)
+        assert item_response.value == value
+
+    async def subscribe_happy_path_string_with_discontinuity(
+        topic_client_async: TopicClientAsync, cache_name: str
+    ) -> None:
+        topic = uuid_str()
+        value = uuid_str()
+
+        _ = await topic_client_async.publish(cache_name, topic_name=topic, value=value)
+
+        subscribe_response = await topic_client_async.subscribe(
+            cache_name, topic_name=topic, resume_at_topic_sequence_number=5, resume_at_topic_sequence_page=5
+        )
+        assert isinstance(subscribe_response, TopicSubscribe.SubscriptionAsync)
+
+        item_task = subscribe_response.__anext__()
         item_response = await item_task
         assert isinstance(item_response, TopicSubscriptionItem.Text)
         assert item_response.value == value
 
     async def subscribe_happy_path_binary(
-        client: CacheClientAsync, topic_client_async: TopicClientAsync, cache_name: str
+        topic_client_async: TopicClientAsync, cache_name: str
     ) -> None:
         topic = uuid_str()
         value = uuid_bytes()
@@ -86,7 +123,7 @@ def describe_subscribe() -> None:
         assert item_response.value == value
 
     async def succeeds_with_nonexistent_topic(
-        client: CacheClientAsync, topic_client_async: TopicClientAsync, cache_name: str
+        topic_client_async: TopicClientAsync, cache_name: str
     ) -> None:
         topic = uuid_str()
 
