5f1398fc45551c4afbb4180395e0166872a63f8c
pgautier404 <pgautier404@users.noreply.github.com>
2025-04-23 14:58:35 -0700

    chore: fixed timeout retry strategy (#504)
    * chore: adding fixed timeout retry strategy
    
    * chore: grab client timeout out of client call details
    
    * chore: update synch retry interceptor
    
    * chore: linting
    
    * chore: remove unnecessary decoding logic and leave a comment
    
    * chore: add comment about client call details value

--- src/momento/internal/synchronous/_retry_interceptor.py
+++ src/momento/internal/synchronous/_retry_interceptor.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import logging
 import time
+from datetime import datetime, timedelta
 from typing import Callable, TypeVar
 
 import grpc
@@ -35,8 +36,28 @@ class RetryInterceptor(grpc.UnaryUnaryClientInterceptor):
         client_call_details: grpc.ClientCallDetails,
         request: RequestType,
     ) -> InterceptorCall | ResponseType:
+        call = None
         attempt_number = 1
+        # The overall deadline is calculated from the timeout set on the client call details.
+        # That value is set in our gRPC configurations and, while typed as optional, will never be None here.
+        overall_deadline = datetime.now() + timedelta(seconds=client_call_details.timeout or 0.0)
+        # variable to capture the penultimate call to a deadline-aware retry strategy, which
+        # will hold the call object before a terminal DEADLINE_EXCEEDED response is returned
+        last_call = None
+
         while True:
+            if attempt_number > 1:
+                retry_deadline = self._retry_strategy.calculate_retry_deadline(overall_deadline)
+                if retry_deadline is not None:
+                    client_call_details = grpc.aio._interceptor.ClientCallDetails(
+                        client_call_details.method,
+                        retry_deadline,
+                        client_call_details.metadata,
+                        client_call_details.credentials,
+                        client_call_details.wait_for_ready,
+                    )
+                    last_call = call
+
             call = continuation(client_call_details, request)
             response_code = call.code()  # type: ignore[attr-defined]  # noqa: F401
 
@@ -44,11 +65,13 @@ class RetryInterceptor(grpc.UnaryUnaryClientInterceptor):
                 return call
 
             retryTime = self._retry_strategy.determine_when_to_retry(
-                RetryableProps(response_code, client_call_details.method, attempt_number)
+                # Note: the async interceptor gets `client_call_details.method` as a binary string that needs to be decoded
+                # but the sync interceptor gets it as a string.
+                RetryableProps(response_code, client_call_details.method, attempt_number, overall_deadline)
             )
 
             if retryTime is None:
-                return call
+                return last_call or call
 
             attempt_number += 1
             time.sleep(retryTime)
